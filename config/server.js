31a095a2eaa4a1c81002bb03d4ee4eb097de7d14
李萌 <lim16@mingyuanyun.com>
2020-12-15 15:02:26 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '348b8a919376268f688f254805f07430'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '32d51e46-4832-40c7-8e7f-a2f6b9726650'
+};
